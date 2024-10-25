acf28828247c91e6faa0d34ea8ccd9a357431482
ItZmeAc3 <153344910+AceAkira1017@users.noreply.github.com>
2024-10-25 21:37:47 +0800

    Update shoti.js

--- scraper/shoti.js
+++ scraper/shoti.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const TinyURL = require('tinyurl');
 
 exports.config = {
     name: 'Shoti',
@@ -11,16 +12,22 @@ exports.config = {
 exports.initialize = async function ({ req, res, log }) {
     try {
         const { data } = await axios.get(`https://shoti-server-v2.onrender.com/api/v1/request-f`);
+        
+        const originalUrl = data.data?.url || "URL not available";
+
+        const shortenedUrl = await TinyURL.shorten(originalUrl);
+
         res.json({
-          code: data.code,
-          status: data.message,
-          response: {
-            region: data.data.region,
-            url: data.data.url,
-            title: data.data.title,
-            username: data.data.user.username,
-            nickname: data.data.user.nickname
-          }
+            code: data.code,
+            status: data.message,
+            response: {
+                region: data.data?.region || "Unknown",
+                url: shortenedUrl || originalUrl,
+                title: data.data?.title || "No title",
+                username: data.data?.user?.username || "Unknown user",
+                nickname: data.data?.user?.nickname || "No nickname"
+            },
+            author: "AceGerome"
         });
 
     } catch (error) {
@@ -30,23 +37,3 @@ exports.initialize = async function ({ req, res, log }) {
         });
     }
 };
-
-/*
-{
-  "code": 200,
-  "message": "success",
-  "data": {
-    "_shoti_rank": "ERR_METHOD_NOT_REQUIRE_KEY",
-    "region": "PH",
-    "url": "https://www.tikwm.com/video/media/hdplay/7362942735415840006.mp4",
-    "cover": "https://p16-sign-va.tiktokcdn.com/tos-maliva-p-0068/f4f2649190d64cbeb8a35e70290d69d2_1714318701~tplv-tiktokx-cropcenter:300:400.jpeg?dr=14579&nonce=38972&refresh_token=82331b1e72b64e6980d6b18ab28c5179&x-expires=1729947600&x-signature=N8O7EQYIQg3KuOIMJjxEiyRinb8%3D&idc=maliva&ps=13740610&s=AWEME_DETAIL&shcp=34ff8df6&shp=d05b14bd&t=4d5b0474",
-    "title": "",
-    "duration": "7s",
-    "user": {
-      "username": "yannaa._1",
-      "nickname": "yanaaapogii",
-      "userID": "7188688956056683521"
-    }
-  }
-}
-*/
