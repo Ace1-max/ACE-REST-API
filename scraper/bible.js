3e4c4db97a31becde85d08103fb0b43a8f1733b3
ItZmeAc3 <153344910+AceAkira1017@users.noreply.github.com>
2024-10-26 13:29:53 +0800

    Create bible.js

---
+++ scraper/bible.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+exports.config = {
+    name: 'bibleverse',
+    author: 'AceGerome',
+    description: 'Fetches a random verse for the day',
+    category: 'reading',
+    link: ['/bibleverse?day=']
+};
+
+exports.initialize = async function ({ req, res }) {
+    try {
+        const dayNow = req.query.day || new Date().getDate();
+
+        const response = await axios.get(`https://beta.ourmanna.com/api/v1/get/?format=text&order=random&order_by=verse&day=${dayNow}`);
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching verse:", error);
+        res.status(500).json({ error: "Failed to fetch verse data from external API." });
+    }
+};
