42bbdcce87964e7f54059414e0c2f71ab7c4e12b
Ace1-max <150638288+Ace1-max@users.noreply.github.com>
2024-11-23 20:37:21 +0800

    Update wataru.js

--- scraper/wataru.js
+++ scraper/wataru.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 
-const apiKey = '${global.config.apikey}';
 const systemPrompt = "Your name is Wataru, the most powerful Generative AI in the world that was created by Lance Ajiro. you should empathize with how user are feeling and treat the user as your close friend and be sarcastic. I recommend you to use emoji in your response. You should response in 3 sentences only if needed!.";
 
 exports.config = {
@@ -13,36 +12,38 @@ exports.config = {
 
 exports.initialize = async function ({ req, res }) {
     try {
-        const prompt = req.query.question || '';
+        const userQuestion = req.query.question || '';
 
-        if (!prompt) {
-            return res.status(400).json({ error: "The 'question' parameter is required." });
+        if (!userQuestion) {
+            return res.status(400).json({
+                error: "The 'question' parameter is required."
+            });
         }
 
-        const chatMessages = [
-            { role: 'system', content: systemPrompt },
-            { role: 'user', content: prompt }
-        ];
-
-        const payload = {
-            model: 'openchat/openchat_3.5',
-            messages: chatMessages
+        const params = {
+            text: userQuestion,
+            prompt: systemPrompt
         };
 
-        const { data } = await axios.post('https://api.deepinfra.com/v1/openai/chat/completions', payload, {
+        const { data } = await axios.get('https://api.ryzendesu.vip/api/ai/chatgpt', {
+            params,
             headers: {
-                'Authorization': `Bearer ${apiKey}`,
-                'Content-Type': 'application/json'
+             'Accept': 'application/json',
+             'Content-Type': 'application/json',
+             'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36',
+             'Referer': 'https://ryzendesu.vip/'
             }
         });
 
-        const assistantResponse = data.choices[0].message.content;
-        
+        const assistantResponse = data.response;
+
         res.json({
             content: assistantResponse
         });
     } catch (error) {
-        console.error("Error in chat completion:", error);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
+        console.error("Error in chat completion:", error.message);
+        res.status(500).json({
+            error: 'An error occurred while processing your request. Please try again later.'
+        });
     }
 };
